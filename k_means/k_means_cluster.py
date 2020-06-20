78b414a70eb50fe5192d31542998899e37d439bd
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2020-06-19 23:58:27 -0500

    clustering aka kMeans

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -3,10 +3,6 @@
 """ 
     Skeleton code for k-means clustering mini-project.
 """
-
-
-
-
 import pickle
 import numpy
 import matplotlib.pyplot as plt
@@ -15,6 +11,7 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
+import streamlit as st
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -34,12 +31,12 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.xlabel(f1_name)
     plt.ylabel(f2_name)
     plt.savefig(name)
-    plt.show()
-
+    #plt.show()
+    st.pyplot()
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -53,6 +50,9 @@ features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+import pandas as pd
+df = pd.DataFrame(data_dict)
+df
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
@@ -60,17 +60,37 @@ poi, finance_features = targetFeatureSplit( data )
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
-plt.show()
+#plt.show()
+st.pyplot()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
 
-
-
-### rename the "name" parameter when you change the number of features
-### so that the figure gets saved to a different file
+kmeans = KMeans(n_clusters=2, n_init=10, max_iter=300,random_state=0).fit(finance_features)
+pred = kmeans.predict(finance_features)
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters2.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print("no predictions object named pred found, no clusters to plot")
+
+kmeans = KMeans(n_clusters=3, n_init=10, max_iter=300,random_state=0).fit(finance_features)
+pred = kmeans.predict(finance_features)
+try:
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters3.pdf", f1_name=feature_1, f2_name=feature_2)
+except NameError:
+    print("no predictions object named pred found, no clusters to plot")
+
+
+st.title("Exercised_stock_options")
+x = df.loc['exercised_stock_options',:] = pd.to_numeric(df.loc['exercised_stock_options',:], errors="coerse")
+x = x.dropna()
+st.write(x.max())
+st.write(x.min())
+
+st.title("salary")
+x = df.loc['salary',:] = pd.to_numeric(df.loc['salary',:], errors="coerse")
+x = x.dropna()
+st.write(x.max())
+st.write(x.min())
